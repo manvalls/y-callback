3eeca496c26ad39a4f9f68fc59dd50f8a35f7620
Manuel Valls Fernández <manolo@vlrz.es>
2015-09-06 11:16:41 +0200

    Added 'cb' argument

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 var Resolver = require('y-resolver'),
     Su = require('u-su'),
-    
+
     yielded = Su();
 
 function toYd(){
   return this[yielded];
 }
 
-module.exports = function(){
-  var resolver = new Resolver();
-  
-  function ret(){
+module.exports = function(cb){
+  var resolver = new Resolver(),
+      ret;
+
+  if(cb) ret = function(){
+    try{ resolver.accept(cb.apply(this,arguments)); }
+    catch(e){ resolver.reject(e); }
+  };
+  else ret = function(){
     resolver.accept(arguments);
-  }
-  
+  };
+
   ret[yielded] = resolver.yielded;
   ret[Resolver.toYielded] = toYd;
-  
+
   return ret;
 };
-
